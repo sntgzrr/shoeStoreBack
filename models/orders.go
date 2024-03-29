daa6cd8c65acff2a3eccd23dceb504406e0fbf0d
sntgzrr <santilozanoc077@gmail.com>
2024-03-29 17:37:57 -0500

    Corrections in ordersRepository

--- models/orders.go
+++ models/orders.go
@@ -1,13 +1,18 @@
 package models
 
-import "time"
+import (
+	"github.com/lib/pq"
+	"time"
+)
 
 type Order struct {
-	OrderID        int       `json:"order_id,omitempty"`
-	UserID         User      `json:"user_id,omitempty"`
-	Products       string    `json:"products,omitempty"`
-	OrderAddress   string    `json:"order_address,omitempty"`
-	OrderCreatedAt time.Time `json:"order_created_at,omitempty"`
+	OrderID        int           `json:"order_id,omitempty"`
+	UserID         User          `json:"user_id,omitempty"`
+	Products       pq.Int64Array `json:"products,omitempty"`
+	TotalQuantity  int           `json:"total_quantity,omitempty"`
+	TotalPrice     int           `json:"total_price,omitempty"`
+	OrderAddress   string        `json:"order_address,omitempty"`
+	OrderCreatedAt time.Time     `json:"order_created_at,omitempty"`
 }
 
 type Orders []*Order
--- repositories/ordersRepository.go
+++ repositories/ordersRepository.go
@@ -1,33 +1,20 @@
 package repositories
 
 import (
-	"errors"
 	"storeApiRest/database"
 	"storeApiRest/models"
-	"strconv"
-	"strings"
 )
 
 func CreateOrder(order models.Order) error {
-	queryOrder := `INSERT INTO orders(user_id, order_address)
-					VALUES ($1, $2)`
-	queryOrderProduct := `INSERT INTO order_products(order_id, product_id)
-							VALUES ($1, $2)`
+	queryOrder := `INSERT INTO orders(user_id, order_address) VALUES ($1, $2) RETURNING order_id`
+	queryOrderProduct := `INSERT INTO order_products(order_id, product_id) VALUES ($1, $2)`
 	db := database.GetConnection()
 	defer db.Close()
 
-	stmtOrder, err := db.Prepare(queryOrder)
+	var orderID int
+	err := db.QueryRow(queryOrder, order.UserID.UserID, order.OrderAddress).Scan(&orderID)
 	if err != nil {
 		return err
-	}
-	defer stmtOrder.Close()
-	resultOrder, err := stmtOrder.Exec(order.UserID.UserID, order.OrderAddress)
-	if err != nil {
-		return err
-	}
-	i, _ := resultOrder.RowsAffected()
-	if i != 1 {
-		return errors.New("more than 1 row was affected")
 	}
 
 	stmtOrderProduct, err := db.Prepare(queryOrderProduct)
@@ -35,17 +22,10 @@ func CreateOrder(order models.Order) error {
 		return err
 	}
 	defer stmtOrderProduct.Close()
-
-	products := strings.Split(order.Products, ",")
-	for _, product := range products {
-		id, _ := strconv.Atoi(product)
-		resultOrderProduct, err := stmtOrderProduct.Exec(order.OrderID, id)
+	for _, product := range order.Products {
+		_, err := stmtOrderProduct.Exec(orderID, product)
 		if err != nil {
 			return err
-		}
-		i, _ = resultOrderProduct.RowsAffected()
-		if i != 1 {
-			return errors.New("more than 1 row was affected")
 		}
 	}
 	return nil
@@ -55,12 +35,14 @@ func ReadOrders() (models.Orders, error) {
 	var orders models.Orders
 	query := `SELECT o.order_id, o.order_address, o.order_created_at,
     			u.user_id, u.user_full_name,
-    			STRING_AGG(CAST(p.product_id AS VARCHAR), ', ') AS products
+    			ARRAY_AGG(p.product_id) AS products,
+    			 SUM(op.quantity) AS total_quantity,
+    			 SUM(op.quantity * p.product_price) AS total_price
 				FROM orders o
          			JOIN order_products op ON o.order_id = op.order_id
          			JOIN products p ON op.product_id = p.product_id
          			JOIN users u ON o.user_id = u.user_id
-						GROUP BY o.order_id, o.order_address, o.order_created_at, u.user_id, u.user_full_name`
+						GROUP BY o.order_id, u.user_id`
 	db := database.GetConnection()
 	defer db.Close()
 	rows, err := db.Query(query)
@@ -75,6 +57,8 @@ func ReadOrders() (models.Orders, error) {
 			&order.OrderID, &order.OrderAddress, &order.OrderCreatedAt,
 			&user.UserID, &user.UserFullName,
 			&order.Products,
+			&order.TotalQuantity,
+			&order.TotalPrice,
 		)
 		if err != nil {
 			return nil, err
